fed4387e2717df67095b4fc19556f2443cd8a0c2
Ismail Elouafiq <ismail.elouafiq@telecom-bretagne.eu>
2015-01-19 10:42:59 +0100

    manual input

--- fradar.py
+++ fradar.py
@@ -8,19 +8,37 @@ Created on Fri Jan 16 10:13:32 2015
 @author: ielouafiq
 """
 import requests
-latitude = 43
-longitude = 7
-bound = 1
-default_url = "http://arn.data.fr24.com/zones/fcgi/feed.js?"
+import futilities as FLUT
+default_url = FLUT.default_url
 
-def compute_bounds(lat, lon, R):
-        lat1, lon1 = lat+R, lon-R
-        lat2, lon2 = lat-R, lon+R
-        return lat1, lat2, lon1, lon2
+# logging settings
+import logging
+import datetime
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+                    datefmt='%m-%d %H:%M',
+                    filename=FLUT.LOG_FILE,
+                    filemode='a')
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+console.setFormatter(logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s'))
+logging.getLogger('').addHandler(console)
+logging.debug("\n\n   =====  ===== STARTED :"+__file__+" ON :"+
+    str((datetime.datetime.now().time()).isoformat()) + "   =====  =====")
+    
+
         
 if __name__=="__main__":
-    bounds = compute_bounds(latitude, longitude, bound)
+    try:
+        latitude, longitude, bound = [long(entry) for entry in 
+        raw_input("Enter Latitude, Longitude and Bound seperated by a space:\n").strip().split()]
+    except Exception, e:
+        logging.error(e)
+        logging.warning("Used default (lat, lon, bound)")
+        latitude, longitude, bound = FLUT.default_latitude, FLUT.default_longitude, FLUT.default_bound
+        
+    bounds = FLUT.compute_bounds(latitude, longitude, bound)
     req_url = default_url+"bounds="+",".join([str(elt) for elt in bounds])
     
     req=requests.get(req_url)
-    print req.text
+    logging.info(req.json())
--- futilities.py
+++ futilities.py
@@ -6,6 +6,21 @@ Created on Mon Jan 19 10:23:57 2015
 
 @author: ielouafiq
 """
+# Log File location
+LOG_FILE 	 = "temp\.log_file.log"
+# default values for url, lat, lon, bound
+default_url = "http://arn.data.fr24.com/zones/fcgi/feed.js?"
+default_latitude = 43
+default_longitude = 7
+default_bound = 1
 def distance(lat1, lat2):
-    'Return distance in miles between two lats'
+    """Return distance in miles between two lats
+    """
     return 69*abs(lat1-lat2)
+    
+def compute_bounds(lat, lon, R):
+    """Compute bounds given lat, lon and radius
+    """
+    lat1, lon1 = lat+R, lon-R
+    lat2, lon2 = lat-R, lon+R
+    return lat1, lat2, lon1, lon2
