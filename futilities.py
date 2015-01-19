bbc52daa4905a182e368ed719ee5d0f0e9b74a55
Ismail Elouafiq <ismail.elouafiq@telecom-bretagne.eu>
2015-01-19 10:26:40 +0100

    add distance measure

---
+++ futilities.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Flight Utilities
+
+Created on Mon Jan 19 10:23:57 2015
+
+@author: ielouafiq
+"""
+def distance(lat1, lat2):
+    'Return distance in miles between two lats'
+    return 69*abs(lat1-lat2)
