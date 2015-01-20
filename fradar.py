58d79e821b3728fd1e99a38d8d70c6ac9d034fe4
Ismail Elouafiq <ismail.elouafiq@telecom-bretagne.eu>
2015-01-20 15:01:50 +0100

    Added GreatCircle Distance + Non french address handling

--- fradar.py
+++ fradar.py
@@ -12,6 +12,7 @@ import time
 import requests
 import futilities as FLUT
 from geopy.geocoders import Nominatim
+from geopy.distance import great_circle
 
 default_url = FLUT.default_url
 
@@ -31,13 +32,12 @@ logging.debug("\n\n   =====  ===== STARTED :"+__file__+" ON :"+
     str((datetime.datetime.now().time()).isoformat()) + "   =====  =====")
     
 def monitor_flights(lat, lon, bound):
-    bounds = FLUT.compute_bounds(latitude, longitude, bound)
+    bounds = FLUT.compute_bounds(lat, lon, bound)
     str_bounds = ",".join([str(elt) for elt in bounds])
     req_url = default_url+"bounds="+str_bounds
     
     req=requests.get(req_url)    
     flights = req.json()
-    print flights.items()
     print '-'*50
     print ' Bounds: '+str_bounds+' Full count: '+str(flights["full_count"])
     print '-'*50
@@ -45,9 +45,8 @@ def monitor_flights(lat, lon, bound):
         if isinstance(data, list) and len(data)>=13:
             depAirport, arrAirport = data[11], data[12]
             flightNumber, acType = data[13], data[8]
-            distance = FLUT.distance(lat, data[1])
             print "- "+flightNumber+" | "+depAirport+"-->"+arrAirport+" | Aircraft: "+acType
-            print "  _ distance from location: "+str(distance)
+            print "  _ Great-Circle Distance from location:      "+str(great_circle( (lat, lon), (data[1], data[2]) ))
     print '-'*50
     
         
@@ -55,9 +54,11 @@ if __name__=="__main__":
     geolocator = Nominatim()
     try:
         location = geolocator.geocode(raw_input("Enter location here: "))
-        print "- Chosen location:", location.address
         latitude, longitude,  = location.latitude, location.longitude
-        bound = int(raw_input("Enter max distance from boundaries: "))
+        print "\nChosen location is in: ( latitude:", latitude, ", longitude:", longitude," )"
+        bound = int(raw_input("\nEnter max lat/lon difference from boundaries: "))
+        if bound<0:
+            bound = FLUT.default_bound
     except Exception, e:
         logging.error(e)
         logging.warning("Used default (lat, lon, bound)")
